246cdb141cc77b0a6ca2d26ea6b29a874e354be6
rampotown <balyubar@buffalo.edu>
2021-01-29 17:01:32 -0500

    Added Player Controller
    Added player controller
    Created pickup system
    Created post processing and lighting volumes

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float step;
+    public float itemWeight;
+
+    Grabber playerGrabber;
+    Animator anim;
+
+    private void Start()
+    {
+        itemWeight = 0;
+
+        anim = GetComponent<Animator>();
+        playerGrabber = GetComponentInChildren<Grabber>();
+    }
+
+    void Update()
+    {
+        Controls();
+        CheckForGrabber();
+    }
+
+    private void Controls()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            walking(-90);
+        }
+
+        else if (Input.GetKey(KeyCode.A))
+        {
+            walking(180);
+        }
+
+        else if (Input.GetKey(KeyCode.S))
+        {
+            walking(90);
+        }
+
+        else if (Input.GetKey(KeyCode.D))
+        {
+            walking(0);
+        }
+
+        if(!Input.GetKey(KeyCode.W) &&
+            !Input.GetKey(KeyCode.A) &&
+            !Input.GetKey(KeyCode.S) &&
+            !Input.GetKey(KeyCode.D))
+        {
+            if (!playerGrabber.Grabbed)
+            {
+                anim.SetBool("Carrying Idle", false);              
+                anim.SetBool("Idle", true);
+            }
+
+            if (playerGrabber.Grabbed)
+            {
+                anim.SetBool("Carrying Idle", true);
+                anim.SetBool("Idle", false);             
+            }
+
+            anim.SetBool("Walking", false);
+            anim.SetBool("Carrying Walking", false);
+        }
+    }
+
+    private void CheckForGrabber()
+    {
+        if (!playerGrabber.Grabbed && playerGrabber.isGrabbable && Input.GetKeyDown(KeyCode.F))
+        {
+            playerGrabber.Grab();
+        }
+
+        else if (playerGrabber.Grabbed && Input.GetKeyDown(KeyCode.F))
+        {
+            playerGrabber.Drop();
+        }
+
+        itemWeight = playerGrabber.GrabbedObjectWeight;
+    }
+
+    private void walking(float zRotation)
+    {
+        float speed = (step - itemWeight) * Time.deltaTime;
+        transform.localRotation =  Quaternion.Euler(0, zRotation, 0);
+        transform.Translate(Vector3.forward * speed);
+
+        if (!playerGrabber.Grabbed)
+        {
+            anim.SetBool("Walking", true);
+            anim.SetBool("Idle", false);
+            anim.SetBool("Carrying Idle", false);
+            anim.SetBool("Carrying Walking", false);
+        }
+
+        if (playerGrabber.Grabbed)
+        {
+            anim.SetBool("Walking", false);
+            anim.SetBool("Idle", false);
+            anim.SetBool("Carrying Idle", false);
+            anim.SetBool("Carrying Walking", true);
+        }
+    }
+}
