22ae032db1a23b1e5dc0af72ae98874099fd74b6
Anthony Wong <anthonywong555@gmail.com>
2021-01-29 21:16:17 -0500

    Add button press sounds and seagulls

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,6 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
 
+    public AudioSource audioSource;
+    public AudioClip audioClip;
+    
+    public void playMenuButtonPress() {
+        audioSource.clip = audioClip;
+        audioSource.Play();
+    }
+
     public void PlayGame() {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
