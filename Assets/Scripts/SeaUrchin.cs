409712786c867ef7d3d0166aed998b559a2b3242
rampotown <balyubar@buffalo.edu>
2021-01-30 18:17:15 -0500

    Added sea urchin

---
+++ Assets/Scripts/SeaUrchin.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeaUrchin : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision c)
+    {
+        if (c.gameObject.CompareTag("Player")) {
+
+            Vector3 dir = c.contacts[0].point - transform.position;
+            dir = -dir.normalized;
+            c.gameObject.GetComponent<Rigidbody>().AddForce(dir * 5000);
+            c.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.up * 12000);
+        }
+    }
+}
