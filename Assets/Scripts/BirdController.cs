ef84b1bba16082192408b0718cd261f6316821d0
rampotown <balyubar@buffalo.edu>
2021-01-29 22:51:10 -0500

    Added Bird Controller
    -Add bird controller functionality
    -Added pooping
    -Added poop targeting
    -Added poop effects

---
+++ Assets/Scripts/BirdController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdController : MonoBehaviour
+{
+    [SerializeField]
+    private Transform birdAI;
+
+    [SerializeField]
+    private Transform poopSpawnPoint;
+    [SerializeField]
+    private GameObject poop;
+
+    [SerializeField]
+    private float speed;
+    private bool canPoop = true;
+
+    [SerializeField]
+    float poopingSpeed = 1;
+
+    [Header("Waypoint")]
+
+    [SerializeField]
+    private Vector3 pointA;
+    [SerializeField]
+    private Vector3 pointB;
+
+    void Update()
+    {
+        SetBirdPosition();
+        Poop();
+    }
+
+    private void SetBirdPosition()
+    {            
+        birdAI.position = Vector3.Lerp(pointA, pointB, Mathf.PingPong(Time.time * speed, 1));
+    }
+
+    private void Poop()
+    {
+        if(canPoop)
+        {
+            StartCoroutine(SpawnPoop());    
+        }
+    }
+
+    IEnumerator SpawnPoop()
+    {
+        canPoop = false;
+        Instantiate(poop, poopSpawnPoint.position, poopSpawnPoint.rotation);
+        yield return new WaitForSeconds(poopingSpeed);
+        canPoop = true;
+    }
+}
---
+++ Assets/Scripts/Poop.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Poop : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject poopSplatter;
+
+    [SerializeField]
+    private GameObject poopTargeter;
+    GameObject poopTargeterObj;
+
+    private RaycastHit targeter;
+
+    private void Start()
+    {
+        if (Physics.Raycast(transform.position, -Vector3.up, out targeter))
+        {
+            if (targeter.collider)
+            {
+                poopTargeterObj = Instantiate(poopTargeter, targeter.point, transform.rotation) as GameObject;
+            }
+        }      
+    }
+
+    private void Update()
+    {
+        Destroy(gameObject, 2);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject poopSplatterParticleObj = Instantiate(poopSplatter, transform.position, transform.rotation);
+
+        Destroy(poopTargeterObj);
+
+        Destroy(poopSplatterParticleObj, 2);
+        Destroy(gameObject);
+    }
+}
