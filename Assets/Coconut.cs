2834b4c8960bf7826ea3eacbb61003d0e87408f9
rampotown <balyubar@buffalo.edu>
2021-01-31 14:56:20 -0500

    Added Monkey

---
+++ Assets/Coconut.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coconut : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject coconutParticles;
+
+    private void OnCollisionEnter(Collision other)
+    {
+        GameObject coconutParticlesObj = Instantiate(coconutParticles, transform.position, transform.rotation);
+
+        if (other.gameObject.CompareTag("Player"))
+        {
+            PlayerController playerController = other.gameObject.GetComponent<PlayerController>();
+            playerController.GetHit();
+            AudioSource audioSource = coconutParticlesObj.GetComponent<AudioSource>();
+            audioSource.Play();
+        }
+
+        Destroy(coconutParticlesObj, 2);
+        Destroy(gameObject);
+    }
+}
--- Assets/Monkey.cs
+++ Assets/Monkey.cs
@@ -10,6 +10,13 @@ public class Monkey : MonoBehaviour
     [SerializeField]
     private GameObject Coconut;
 
+    [SerializeField]
+    private Transform handAnchor;
+
+    private float exitTime = 0;
+    private float throwSpeed = 12;
+    private float upwardsForce = 2;
+
     private void Update()
     {
         if (canThrow)
@@ -21,24 +28,34 @@ public class Monkey : MonoBehaviour
 
     private IEnumerator ThrowObject()
     {
-       Pickup();
 
-       yield return new WaitForSeconds(1.2f);
+        yield return new WaitForSeconds(4.767f + exitTime); //length of dance animation and buffer
+
+        Pickup();
+
+       yield return new WaitForSeconds(.8f);
 
        Throw();
 
-       yield return new WaitForSeconds(1);
+       yield return new WaitForSeconds(1.4f + exitTime);
 
        canThrow = true;
     }
 
+    private GameObject coconutObj;
+
     private void Pickup()
     {
-      //  Instantiate(Coconut, )
+       coconutObj = Instantiate(Coconut, handAnchor.position, handAnchor.rotation) as GameObject;
+       coconutObj.transform.SetParent(handAnchor);
     }
 
       private void Throw()
     {
-
+        Rigidbody rb = coconutObj.GetComponent<Rigidbody>();
+        coconutObj.transform.SetParent(null);
+        rb.isKinematic = false;
+        rb.AddForce(transform.forward * throwSpeed, ForceMode.Impulse);
+        rb.AddForce(transform.up * upwardsForce, ForceMode.Impulse);
     }
 }
