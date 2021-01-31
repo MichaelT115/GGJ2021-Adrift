74744ac9742a4203bcc71d5936760784b68c11d1
Michael Cooper <MTCooper115@gmail.com>
2021-01-31 00:53:59 -0500

    Implement Toast messages.

---
+++ Assets/MessageGenerator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+using Random = UnityEngine.Random;
+
+public class MessageGenerator : MonoBehaviour
+{
+	[SerializeField]
+	private float countdown = 5;
+	[SerializeField]
+	private float minimumTime = 3;
+	[SerializeField]
+	private float maximumTime = 8;
+
+	[Serializable]
+	private class MessageEvent : UnityEvent<string> { }
+
+	[SerializeField]
+	private MessageEvent OnMessage;
+
+	[SerializeField]
+	private string[] messages;
+
+	private void Update()
+	{
+		countdown -= Time.deltaTime;
+
+		if (countdown <= 0)
+		{
+			countdown += Toast.TOAST_TIME + Random.Range(minimumTime, maximumTime);
+			string message = RandomMessage;
+
+			OnMessage.Invoke(message);
+			Debug.Log($"Message: {message}");
+		}
+	}
+
+	private string RandomMessage => messages[Random.Range(0, messages.Length)];
+}
---
+++ Assets/Toast.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Toast : MonoBehaviour
+{
+	public const float TOAST_TIME = 7f;
+	private const float REVEAL_TIME = 1f;
+
+	[SerializeField]
+    private RectTransform toast;
+
+	[SerializeField]
+	private float time;
+
+	[SerializeField]
+	private TMPro.TMP_Text text;
+
+	private void Start()
+	{
+		NormalizePosition = 0;
+		enabled = false;
+	}
+
+	public void DisplayMessage(string message)
+	{
+		text.text = message;
+		time = 0;
+		enabled = true;
+	}
+
+	private void Update()
+	{
+		time += Time.deltaTime;
+
+		if (time < REVEAL_TIME)
+		{
+			NormalizePosition = time / REVEAL_TIME;
+		}
+		else if (time < (TOAST_TIME - REVEAL_TIME))
+		{
+			NormalizePosition = 1;
+		}
+		else
+		{
+			float timeIntoAnimation = time - (TOAST_TIME - REVEAL_TIME);
+			float normalizedTimeIntoAnimation = timeIntoAnimation / REVEAL_TIME;
+			NormalizePosition = 1 - normalizedTimeIntoAnimation;
+
+			if (NormalizePosition == 0)
+			{
+				enabled = false;
+			}
+		}
+	}
+
+	public float NormalizePosition
+	{
+		get => toast.anchoredPosition.y / toast.rect.height;
+		set
+		{
+			var y = -toast.rect.height * Mathf.Clamp01(value);
+			var anchoredPosition = toast.anchoredPosition;
+			anchoredPosition.y = y;
+			toast.anchoredPosition = anchoredPosition;
+
+		}
+	}
+}
